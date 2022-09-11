e568bb02a7e58349bc0093a4a26afaf35f11f224
David Osezua <davidosez78@gmail.com>
2022-09-11 23:58:36 +0100

    numbers

---
+++ app.js
@@ -0,0 +1,97 @@
+/* QUESTION 1. 
+Write a program that generates and prints 50 random integers, each between 3 and 6. */
+
+// SOLUTION
+// function range(x, y) {
+//   let result = Math.floor(Math.random() * (y - x + 1)) + x;
+//   return result;
+// }
+
+// let a = range(3, 6);
+// console.log(a);
+
+/* QUESTION2.
+Write a program that generates a random number, x, between 1 and 50, a random number y
+between 2 and 5, and computes x^y
+. */
+
+//  solution
+// function range(x, y) {
+//   let result = Math.floor(Math.random() * (y - x + 1)) + x;
+//   return result;
+// }
+
+// let a = range(1, 50);
+// let b = range(2, 5);
+// let result = a ** b;
+// console.log(result);
+
+/* QUESTION 3. 
+Write a program that generates a random number between 1 and 10 and prints your name
+that many times.
+ */
+
+// SOLUTION
+// function range(x, y) {
+//   let result = Math.floor(Math.random() * (y - x + 1)) + x;
+//   for (let i = 0; i < result; i++) {
+//     console.log("david");
+//   }
+// }
+
+// range(1, 10);
+
+/* QUESTION 4. 
+Write a program that generates a random decimal number between 1 and 10 with two decimal
+places of accuracy. Examples are 1.23, 3.45, 9.80, and 5.00.
+ */
+
+// SOLUTION
+// function range(x, y) {
+//   let result = Math.floor(Math.random() * (y - x + 1)) + x;
+//   return result;
+// }
+
+// let a = range(1, 10);
+// let b = range(1, 10);
+// let c = range(1, 10);
+// let result = a + b / 10 + c / 100;
+// console.log(result.toFixed(2));
+
+/* QUESTION 5. 
+Write a program that generates 50 random numbers such that the first number is between 1 and 2, the second is between 1 and 3, the third is between 1 and 4, . . . , and the last is between
+1 and 51.
+*/
+
+// SOLUTION
+// function range(x, y) {
+//   let result = Math.floor(Math.random() * (y - x + 1)) + x;
+//   return result;
+// }
+
+// let a = range(1, 2);
+// let b = range(1, 3);
+// let c = range(1, 4);
+// let d = range(1, 51);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// for (let i = 0; i < 46; i++) {
+//   let i = range(1, 46);
+//   console.log(i);
+// }
+
+// console.log(d);
+
+// function lovefunc(flower1) {
+//   let product = 1;
+//   for (let i = 0; i < flower1.length; i++) {
+//     product *= flower1[i];
+//   }
+//   return product;
+// }
+
+// let s = lovefunc([1, 2, 3, 4, 5, 6, 4, 3, 2, 1]);
+// console.log(s);
+
